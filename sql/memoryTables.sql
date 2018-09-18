a504f50226dd1c0bf2029249113ab948c9f2da51
Stefano Gessa <stefano@gessa.net>
2018-09-18 16:19:19 +0700

    adjust LWF address length for SQL columns

--- sql/memoryTables.sql
+++ sql/memoryTables.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS "mem_accounts"(
   "secondSignature" SMALLINT DEFAULT 0,
   "u_secondSignature" SMALLINT DEFAULT 0,
   "u_username" VARCHAR(20),
-  "address" VARCHAR(22) NOT NULL UNIQUE PRIMARY KEY,
+  "address" VARCHAR(24) NOT NULL UNIQUE PRIMARY KEY,
   "publicKey" BYTEA,
   "secondPublicKey" BYTEA,
   "balance" BIGINT DEFAULT 0,
@@ -46,7 +46,7 @@ CREATE TRIGGER trg_memaccounts_update
 
 
 CREATE TABLE IF NOT EXISTS "mem_round"(
-  "address" VARCHAR(22),
+  "address" VARCHAR(24),
   "amount" BIGINT,
   "delegate" VARCHAR(64),
   "blockId" VARCHAR(20),
@@ -57,7 +57,7 @@ CREATE INDEX IF NOT EXISTS "mem_round_address" ON "mem_round"("address");
 CREATE INDEX IF NOT EXISTS "mem_round_round" ON "mem_round"("round");
 
 CREATE TABLE IF NOT EXISTS "mem_accounts2delegates"(
-  "accountId" VARCHAR(22) NOT NULL,
+  "accountId" VARCHAR(24) NOT NULL,
   "dependentId" VARCHAR(64) NOT NULL,
   FOREIGN KEY ("accountId") REFERENCES mem_accounts("address") ON DELETE CASCADE
 );
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS "mem_accounts2delegates"(
 CREATE INDEX IF NOT EXISTS "mem_accounts2delegates_accountId" ON "mem_accounts2delegates"("accountId");
 
 CREATE TABLE IF NOT EXISTS "mem_accounts2u_delegates"(
-  "accountId" VARCHAR(22) NOT NULL,
+  "accountId" VARCHAR(24) NOT NULL,
   "dependentId" VARCHAR(64) NOT NULL,
   FOREIGN KEY ("accountId") REFERENCES mem_accounts("address") ON DELETE CASCADE
 );
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS "mem_accounts2u_delegates"(
 CREATE INDEX IF NOT EXISTS "mem_accounts2u_delegates_accountId" ON "mem_accounts2u_delegates"("accountId");
 
 CREATE TABLE IF NOT EXISTS "mem_accounts2multisignatures"(
-  "accountId" VARCHAR(22) NOT NULL,
+  "accountId" VARCHAR(24) NOT NULL,
   "dependentId" VARCHAR(64) NOT NULL,
   FOREIGN KEY ("accountId") REFERENCES mem_accounts("address") ON DELETE CASCADE
 );
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS "mem_accounts2multisignatures"(
 CREATE INDEX IF NOT EXISTS "mem_accounts2multisignatures_accountId" ON "mem_accounts2multisignatures"("accountId");
 
 CREATE TABLE IF NOT EXISTS "mem_accounts2u_multisignatures"(
-  "accountId" VARCHAR(22) NOT NULL,
+  "accountId" VARCHAR(24) NOT NULL,
   "dependentId" VARCHAR(64) NOT NULL,
   FOREIGN KEY ("accountId") REFERENCES mem_accounts("address") ON DELETE CASCADE
 );
